d915ca39fbba7804f3aa474206f77b88662cf03d
Anirudra Kisku <kisku.anirudra@gmail.com>
2026-02-21 22:27:49 +0530

    nothing

---
+++ frontend/expence-tracker/src/context/UserContext.jsx
@@ -0,0 +1,63 @@
+import React,{ createContext, useState} from 'react';
+import { children } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) =>{
+    const [user, setUser]= useState(null);
+
+    //Function to update user data
+    const updateUser = (userData)=>{
+        setUser(userData);
+    };
+
+    //Function to clean user data (e.g., on logout)
+    const clearUser = () =>{
+        setUser(null);
+    };
+
+    return(
+        <UserContext.Provider
+            value={{
+                user,
+                updateUser,
+                clearUser,
+            }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
+};
+
+export default UserProvider;
+
+
+// import { createContext, useState } from "react";
+
+// export const UserContext = createContext();
+
+// const UserProvider = ({ children }) => {
+//   const [user, setUser] = useState({}); // not null
+
+//   const updateUser = (userData) => {
+//     setUser(userData);
+//   };
+
+//   const clearUser = () => {
+//     setUser({});
+//   };
+
+//   return (
+//     <UserContext.Provider
+//       value={{
+//         user,
+//         updateUser,
+//         clearUser,
+//       }}
+//     >
+//       {children}
+//     </UserContext.Provider>
+//   );
+// };
+
+// export default UserProvider;
