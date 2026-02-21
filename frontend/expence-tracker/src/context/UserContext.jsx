71377ce3c02ee2a6718402a230583a9858e16dcf
Anirudra Kisku <kisku.anirudra@gmail.com>
2026-02-22 01:12:28 +0530

    UserContext Change

--- frontend/expence-tracker/src/context/UserContext.jsx
+++ frontend/expence-tracker/src/context/UserContext.jsx
@@ -32,32 +32,4 @@ const UserProvider = ({ children }) =>{
 export default UserProvider;
 
 
-// import { createContext, useState } from "react";
 
-// export const UserContext = createContext();
-
-// const UserProvider = ({ children }) => {
-//   const [user, setUser] = useState({}); // not null
-
-//   const updateUser = (userData) => {
-//     setUser(userData);
-//   };
-
-//   const clearUser = () => {
-//     setUser({});
-//   };
-
-//   return (
-//     <UserContext.Provider
-//       value={{
-//         user,
-//         updateUser,
-//         clearUser,
-//       }}
-//     >
-//       {children}
-//     </UserContext.Provider>
-//   );
-// };
-
-// export default UserProvider;
