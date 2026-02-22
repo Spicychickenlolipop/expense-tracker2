64d98e1db5fca1308ae9f71f2e0aaae9f73e807c
Anirudra Kisku <kisku.anirudra@gmail.com>
2026-02-22 10:07:33 +0530

    AuthLayout and SignUp page change

--- frontend/expence-tracker/src/components/layouts/AuthLayout.jsx
+++ frontend/expence-tracker/src/components/layouts/AuthLayout.jsx
@@ -3,11 +3,20 @@ import CARD_2 from '../../assets/images/card2.png';
 import { LuTrendingUpDown } from 'react-icons/lu';
 
 const AuthLayout = ({children}) => {
-  return <div className='flex'>
-    <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
-        <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+//   return <div className='flex'>
+//     <div className="w-screen h-screen md:w-[60vw] px-12 pt-8 pb-12">
+//         <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+//         {children}
+//     </div>
+    return <div className="min-h-screen flex flex-col md:flex-row">
+
+      {/* LEFT SIDE */}
+      <div className="w-full md:w-3/5 px-6 md:px-12 pt-8 pb-12 flex flex-col">
+        <h2 className="text-lg font-medium text-black mb-6">
+          Expense Tracker
+        </h2>
         {children}
-    </div>
+      </div>
 
     <div className='hidden md:block w-[40vw] h-screen bg-violet-50 bg-auth-bg-img bg-img bg-cover bg-no-repeat bg-center overflow-hidden p-8 relative'>
         <div className="w-48 h-48 rounded-[40px] bg-purple-600 absolute -top-7 -left-5"/>
